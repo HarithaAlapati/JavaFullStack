9c803d8bb573bd0dc5e5e4b3d520b1f888e3ce52
Haritha <2102hari@gmail.com>
2025-08-16 09:38:18 +0530

    added

--- SpinrgBot/ecommerce-app-rest-api/src/main/java/com/example/ecommerce_app_rest_api/model/Customer.java
+++ SpinrgBot/ecommerce-app-rest-api/src/main/java/com/example/ecommerce_app_rest_api/model/Customer.java
@@ -1,9 +1,13 @@
 package com.example.ecommerce_app_rest_api.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Customer {
@@ -14,13 +18,33 @@ public class Customer {
     private String name;
     private String email;
     private String password;
+    private int age;
     
+    @OneToMany(mappedBy = "customer")
+    private List<Order> orders = new ArrayList<Order>();
+
 //    @OneToMany(mappedBy = "customer")
 //    private List<Order> orders;
 
     // Getters and Setters
     
-    public Customer() {
+    public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Customer() {
     	super();
     }
 
