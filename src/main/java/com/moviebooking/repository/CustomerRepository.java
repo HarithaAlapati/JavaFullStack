2c490a1886455fa67d2758a6ca6b44d3fb0bf177
Haritha <2102hari@gmail.com>
2025-08-18 09:50:32 +0530

    added

--- src/main/java/com/moviebooking/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.moviebooking.repository;
-
-import com.moviebooking.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
--- src/main/java/com/moviebooking/repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.moviebooking.repository;
-
-import com.moviebooking.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
